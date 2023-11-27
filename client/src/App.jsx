8f4fbf67a8b7832caf14e243859bf36fa0d0d39a
Nikhil AL <nikhilal.2703@gmail.com>
2023-11-27 21:57:02 +0530

    resolve routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,29 @@
+import React from "react";
 import Home from "./components/Home";
 import Header from "./components/Header";
 import FrontsCards from "./components/FrontsCards";
 import Footer from "./components/Footer";
+import Login from "./components/Login";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Home/>
-      <FrontsCards/>
-      <Footer/>
+      <Router>
+      <Header />
+      <Home />
+      <FrontsCards />
+      <Footer />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
 
-export default App; 
+export default App;
